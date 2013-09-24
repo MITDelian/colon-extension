9d322b689179986a003f7d37b7bb72f0f115bd26
Delian Asparouhov <me@delian.io>
2013-09-24 12:17:18 -0700

    First version

---
+++ capitalize.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+    console.log( "ready! and this is a test!!" );
+});
+
+function getTextNodesIn(node, includeWhitespaceNodes) {
+    var textNodes = [], whitespace = /^\s*$/;
+
+    function getTextNodes(node) {
+        if (node.nodeType == 3) {
+            if (includeWhitespaceNodes || !whitespace.test(node.nodeValue)) {
+                textNodes.push(node);
+            }
+        } else {
+            for (var i = 0, len = node.childNodes.length; i < len; ++i) {
+                getTextNodes(node.childNodes[i]);
+            }
+        }
+    }
+
+    getTextNodes(node);
+    return textNodes;
+}
